525a2bd33f667bd6df796f08bd86069eb77c8d3c
domekuf <Dome@MBP-di-Domenico.homenet.telecomitalia.it>
2016-03-03 01:00:21 +0100

    ajax
    request

--- js/cod_fiscale.js
+++ js/cod_fiscale.js
@@ -15,23 +15,36 @@ console.log(formData);
 
 cfUrl='http://webservices.dotnethell.it/codicefiscale.asmx/CalcolaCodiceFiscale';
 cfQs='http://webservices.dotnethell.it/codicefiscale.asmx/CalcolaCodiceFiscale?Nome=Domenico&Cognome=Di+Iorio&DataNascita=26%2F04%2F1991&ComuneNascita=Castel+San+Giovanni&Sesso=M';
-
-var xhttp = new XMLHttpRequest();
-
-// Open the connection.
-	xhttp.open("POST", cfUrl, true);
-	
-	// Set up a handler for when the request finishes.
-	xhttp.onload = function () {
-		//alert("open");
-	if (xhttp.status === 200) {
-		$('#'+id_output).val(xhttp.response);
-	} else {
-		$('#'+id_output).val('errore!');
-	}
-	};
-	// Send the Data.
-	xhttp.send(formData);
+$.ajax({
+      type: "POST",
+      url: cfUrl,
+      data: formData,//"Nome=Domenico&Cognome=Di+Iorio&DataNascita=26%2F04%2F1991&ComuneNascita=Castel+San+Giovanni&Sesso=M",
+      dataType: "html",
+      success: function(msg)
+      {
+        console.log(msg);
+      },
+      error: function()
+      {
+        alert("Chiamata fallita, si prega di riprovare...");
+      }
+    });
+// var xhttp = new XMLHttpRequest();
+// 
+// // Open the connection.
+// 	xhttp.open("POST", cfUrl, true);
+// 	
+// 	// Set up a handler for when the request finishes.
+// 	xhttp.onload = function () {
+// 		//alert("open");
+// 	if (xhttp.status === 200) {
+// 		$('#'+id_output).val(xhttp.response);
+// 	} else {
+// 		$('#'+id_output).val('errore!');
+// 	}
+// 	};
+// 	// Send the Data.
+// 	xhttp.send(formData);
 
 
 
